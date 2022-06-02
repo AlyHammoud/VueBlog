4e9b0465946ee1cea76539e68e28267612b42f02
AlyHammoud <99489154+AlyHammoud@users.noreply.github.com>
2022-06-03 02:28:27 +0300

    Loading, and popup modal, emit

---
+++ src/components/Modal.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="modal">
+    <div class="modal-content">
+      <p>{{ props.modalMessage }}</p>
+      <button class="button" @click="closeModal">Close</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["modalMessage"],
+
+  setup(props, { emit }) {
+    const closeModal = () => emit("close-modal");
+
+    return { props, closeModal };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.modal {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 101;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  background-color: rgba(0, 0, 0, 0.7);
+
+  .modal-content {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    border-radius: 8px;
+    width: 300px;
+    padding: 40px 30px;
+    background-color: #fff;
+
+    p {
+      text-align: center;
+    }
+
+    button {
+      align-self: center;
+      color: #fff;
+      background-color: #303030;
+
+      &:hover {
+        background-color: #8b8b8b;
+      }
+    }
+  }
+}
+</style>
--- src/views/ForgotPassword.vue
+++ src/views/ForgotPassword.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="reset-password">
+    <Modal v-if="state.modalActive" @close-modal="closeModal" />
+    <Loading v-if="state.loading" />
     <div class="form-wrap">
       <form class="reset">
         <h2>Reset Password</h2>
         <p>Forgot your password? Enter your email to reset it.</p>
         <div class="inputs">
           <div class="input">
-            <input type="text" placeholder="Email" v-model="state.email">
+            <input type="text" placeholder="Email" v-model="state.email" />
             <email class="icon" />
           </div>
         </div>
@@ -21,34 +23,45 @@
 <script>
 import { reactive } from "@vue/reactivity";
 import email from "../assets/icons/envelope-regular.svg";
+import Modal from "../components/Modal.vue";
+import Loading from "../components/Loading.vue";
 
 export default {
   name: "ForgotPassword",
   components: {
-    email
+    email,
+    Modal,
+    Loading,
   },
 
   setup() {
     const state = reactive({
-      email: null
+      email: null,
+      modalActive: false,
+      modalMessage: "",
+      loading: null
     });
 
-    return { state };
-  },
+    const closeModal = () => {
+      state.modalActive = !state.modalActive;
+      state.email = "";
+    };
 
-}
+    return { state, closeModal };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-.reset-password{
+.reset-password {
   position: relative;
-  .form-wrap{
-    .reset{
-      h2{
+  .form-wrap {
+    .reset {
+      h2 {
         margin-bottom: 8px;
       }
 
-      p{
+      p {
         text-align: center;
         margin-bottom: 32px;
       }
