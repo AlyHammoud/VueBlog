0161444af4a90d7d3298438bd23b08542c6377b8
AlyHammoud <99489154+AlyHammoud@users.noreply.github.com>
2022-06-03 01:30:07 +0300

    login,register, forgot, design only

--- src/views/ForgotPassword.vue
+++ src/views/ForgotPassword.vue
@@ -1,13 +1,58 @@
 <template>
-  <div></div>
+  <div class="reset-password">
+    <div class="form-wrap">
+      <form class="reset">
+        <h2>Reset Password</h2>
+        <p>Forgot your password? Enter your email to reset it.</p>
+        <div class="inputs">
+          <div class="input">
+            <input type="text" placeholder="Email" v-model="state.email">
+            <email class="icon" />
+          </div>
+        </div>
+        <button class="button">Reset</button>
+        <div class="angle"></div>
+      </form>
+      <div class="backgrou"></div>
+    </div>
+  </div>
 </template>
 
 <script>
+import { reactive } from "@vue/reactivity";
+import email from "../assets/icons/envelope-regular.svg";
+
 export default {
+  name: "ForgotPassword",
+  components: {
+    email
+  },
+
+  setup() {
+    const state = reactive({
+      email: null
+    });
+
+    return { state };
+  },
 
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+.reset-password{
+  position: relative;
+  .form-wrap{
+    .reset{
+      h2{
+        margin-bottom: 8px;
+      }
 
+      p{
+        text-align: center;
+        margin-bottom: 32px;
+      }
+    }
+  }
+}
 </style>
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,13 +1,83 @@
 <template>
-  <div></div>
+  <div class="form-wrap">
+    <form class="register">
+      <p class="login-register">
+        Already have an account? 
+        <router-link class="router-link" :to="{ name: 'Login' }"
+          >Login</router-link
+        >
+      </p>
+
+      <h2>Create your FireBlog account!</h2>
+      <div class="inputs">
+        <div class="input">
+          <input type="text" placeholder="First Name" v-model="state.firstName" />
+          <user class="icon" />
+        </div>
+
+        <div class="input">
+          <input type="text" placeholder="User Name" v-model="state.userName" />
+          <user class="icon" />
+        </div>
+
+        <div class="input">
+          <input type="text" placeholder="Email" v-model="state.email" />
+          <user class="icon" />
+        </div>
+
+        <div class="input">
+          <input type="text" placeholder="Email" v-model="state.email" />
+          <email class="icon" />
+        </div>
+
+        <div class="input">
+          <input type="text" placeholder="Password" v-model="state.password" />
+          <password class="icon" />
+        </div>
+      </div>
+
+      <button class="button">Sign up</button>
+      <div class="angle"></div>
+    </form>
+
+    <div class="background"></div>
+  </div>
 </template>
 
 <script>
+import { reactive } from "@vue/reactivity";
+import email from "../assets/icons/envelope-regular.svg";
+import password from "../assets/icons/lock-alt-solid.svg";
+import user from "../assets/icons/user-alt-light.svg";
 export default {
+  name: "Register",
+  components: {
+    email,
+    password,
+    user,
+  },
 
-}
+  setup() {
+    const state = reactive({
+      email: null,
+      password: null,
+      firstName: null,
+      lastName: null,
+      userName: null,
+    });
+
+    return { state };
+  },
+};
 </script>
 
-<style>
+
+<style lang="scss" scoped>
+
+.register{
+  h2{
+    max-width: 350px;
+  }
+}
 
 </style>
